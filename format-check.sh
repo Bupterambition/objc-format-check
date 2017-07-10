282fa7bb9df7b2849c6a447052dc37a33d842395
bupterambition <bupterambition@gmail.com>
2017-07-10 13:50:45 +0800

    fix:git clone

--- format-check.sh
+++ format-check.sh
@@ -8,7 +8,7 @@ then rm -rf .spacecommander
 fi
 mkdir .spacecommander
 cd .spacecommander
-git clone https://github.com/Bupterambition/objc-format-check.git -branch feature/KEEP spacecommander
+git clone https://github.com/Bupterambition/objc-format-check.git -b feature/KEEP spacecommander
 cd $orgin
 bash "$(pwd)/.spacecommander/spacecommander/setup-repo.sh"
 exit 0
