30e82589d8da51b3a63916256c48b00a7986f2ff
Moon <bupterambition@gmail.com>
2018-04-19 14:00:03 +0800

    Update format-check.sh

--- format-check.sh
+++ format-check.sh
@@ -6,9 +6,9 @@ fi
 if [ -d .spacecommander ];
 then rm -rf .spacecommander
 fi
-mkdir .codeFormat
-cd .codeFormat
-git clone https://github.com/Bupterambition/objc-format-check.git -b feature/KEEP codeFormat
+mkdir .codeFormatForBytedance
+cd .codeFormatForBytedance
+git clone https://github.com/Bupterambition/objc-format-check.git -b feature/KEEP codeFormatForBytedance
 cd $orgin
-bash "$(pwd)/.codeFormat/codeFormat/setup-repo.sh"
+bash "$(pwd)/.codeFormatForBytedance/codeFormatForBytedance/setup-repo.sh"
 exit 0
